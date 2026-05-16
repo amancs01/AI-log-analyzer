25a5764caedcd5e82eb7f8f146d0b648d8c0507f
Aman Singh <singhaman5467@gmail.com>
2026-05-16 23:22:32 +0545

    detect suspicious logs and increase the sample logs

--- ai-log-analyzer/main.py
+++ ai-log-analyzer/main.py
@@ -1,5 +1,27 @@
 log_file_path = "sample_logs.txt"
 
+suspicious_keywords=[
+    "failed login",
+    "failed",
+    "unauthorized",
+    "brute force",
+    "port scan",
+    "malware",
+    "suspicious",
+    "unknown device",
+    "data exfiltration",
+    "high risk"
+    "attack"
+    "blocked"
+
+]
 with open(log_file_path, "r") as file:
     for line in file:
-        print(line.strip())
+        clean_line = line.strip()
+        lower_line = clean_line.lower()
+
+        for keyword in suspicious_keywords:
+            if keyword in lower_line:
+                print("[SUSPICIOUS]", clean_line)
+                break
+           
